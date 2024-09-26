6f9a9adf747fddec155d42712d0c64f9e4cc8321
Your Name <you@example.com>
2024-09-26 15:41:50 +0800

    Fix line endings

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/Blog/",
+  base: "/blogs/",
 
   lang: "zh-CN",
   title: "Thirty_Li",
