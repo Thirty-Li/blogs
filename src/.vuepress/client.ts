e8fbf5c751e65a27019effc2f18f7371f220b2ef
Your Name <you@example.com>
2024-09-26 15:50:45 +0800

    Fix line endings

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,2 +1,4 @@
 import "vuepress-theme-hope/presets/shinning-feature-panel.scss"
 import "vuepress-theme-hope/presets/bounce-icon.scss"
+import "vuepress-theme-hope/presets/hide-navbar-icon.scss"
+import "vuepress-theme-hope/presets/hide-sidebar-icon.scss"
