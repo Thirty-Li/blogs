22d6d2cf496a3bb2596e8d11bb3367ed35637e4f
Your Name <you@example.com>
2024-09-27 17:56:19 +0800

    提交

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,7 +1,20 @@
 import "vuepress-theme-hope/presets/shinning-feature-panel.scss";
 import "vuepress-theme-hope/presets/bounce-icon.scss";
 import { defineEChartsConfig } from "vuepress-plugin-md-enhance/client";
-
+// import { defineClientConfig } from "vuepress/client";
+// import { setupSnowFall } from "vuepress-theme-hope/presets/SnowFall.js";
+//
+// defineClientConfig({
+//     setup() {
+//         setupSnowFall({
+//             speed: 1,
+//             image: "./images/snow.gif",
+//             count: 25,
+//             minSize: 5,
+//             maxSize: 10,
+//         });
+//     },
+// });
 defineEChartsConfig({
     setup: async () => {
         await import("echarts-wordcloud");
